c4235255fe2d34e955c3c09ec70a98c62f16e581
Eijebong <eijebong@myopera.com>
2013-11-29 17:01:11 +0000

    - Premier jet pour les TPL, beaucoup plus simple pour bosser sur du HTML - Correction d'un bug (changement d'une convention mal effectuée)

--- wsgi-scripts/helloworld.py
+++ wsgi-scripts/helloworld.py
@@ -184,60 +184,7 @@ moto = [
 		"This project is YOURS. Bring your ideas, opinions, code, content, …"),
 ]
 
-template = BetterFormat().format("""
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Hello world ☺ - Olissea DEV</title>
-		<meta charset="UTF-8" />
-		<link rel="stylesheet" media="screen" type="text/css" title="Main style" href="css/main.css" />
-	</head>
-	<body>
-		<div id="status"><strong><span class="WIP">WIP</span> - Work In Progress</strong>, <em>come back later!</em></div><!-- # TODO: Remove that? When? -->
-		<div id="extra"><em>Contact me if interested!</em><br /><strong>Contact:</strong> e-warning [at symbol] hotmail.com</div><!-- # TODO: See tag:devutopia.net,2013-11-03:Topic-changing-contact-info (in helloworld.py) -->
-		<div id="title"><strong><span class="dev">Dev</span><span class="utopia">utopia</span></strong></div>
-
-		<hr class="clear" />
-
-		<div id="main">
-			<div id="leftContainer">
-				<h1>Hello World</h1>
-				<div id="helloWorld">
-					{langs}
-					…<br />
-					<button>Add a lang</button> <em>or</em> edit one… (<em>Not implemented yet… Soon!</em>)
-				</div>
-
-				<div id="test">
-					<h2>Testing zone</h2>
-
-					<p>
-						{{test}}
-					</p>
-				</div>
-			</div>
-			<div id="rightContainer">
-				<h1>« A huge collaborative project to come! »</h1>
-				<h2>What is this?</h2>
-				<div class="spoiler">
-					<ol id="moto">
-						{moto}
-					</ol>
-
-					<p>
-					So, your contributions will be automatically accepted under the "By users" tab and will be displayed to the whole world (if you wish), they'll stay there (if you wish), and, in case you proposed data for someone else's content and if you're lucky, it may get merged with the original content by his original author or be choosen to be the new official version.
-					</p>
-				</div>
-			</div>
-			<div class="clear"></div>
-		</div>
-
-		<div id="foot">
-			<span id="privacyPolicy"> We don't collect data about you ☺<span class="heart">♥</span> → We <strong class="love">love</strong> you! <span class="hearts">❤💓💕💖💘💗💙💚💛💜💝💞💟💖💙💜💚💗💘💛💝💞💟</span><!-- # TODO: Ne s'affiche que sous win… Une piste: U+1F495, U+1F496, U+1F497, U+1F499, U+1F49A, U+1F49B, U+1F49C, U+1F49D, U+1F49E, U+1F49F, U+1F496, U+1F497, U+1F498, U+1F49B, U+1F49D, U+1F49E, U+1F49F la séquence complète. C’est valide, et il existe sûrement une fonte qui les a --><!-- Note for myself: If you edit that under windows, it will look glitched, don't edit that part!! --></span>
-			<span id="source"><strong>Source:</strong> ftp://anonymous@devutopia.net/ </span>
-		</div>
-	</body>
-</html>""",
+template = BetterFormat().format(open('tpl/index.tpl').read(4096),
 	# Can't use '<gen> if helloword else "No lang registered yet"' because helloworld returns True (as it is a generator) even if it will not yield anything (which was confusing at first but totaly normal)
 	# So we are using "or" instead
 	langs='\n'.join(
@@ -248,12 +195,13 @@ template = BetterFormat().format("""
 
 	moto='<li>'+'</li>\n<li>'.join('<br />\n'.join(rule) for rule in moto)+'</li>')
 
-test = """\
-<strong>GET:</strong> {get}<br />
-<strong>POST:</strong> {post}<br /><br />
+# Not used
+# test = """\
+# <strong>GET:</strong> {get}<br />
+# <strong>POST:</strong> {post}<br /><br />
 
-<strong>Method:</strong> {method}\
-"""
+# <strong>Method:</strong> {method}\
+# """
 
 # TODO: Continuing studying ATOM feeds.
 rss_template = """\
@@ -421,7 +369,7 @@ class application:
 			authorised_ext = {".css" : "text/css", ".js" : "application/x-javascript"}
 			ext = os.path.splitext(self.environ['PATH_INFO'])[1]
 
-			if ext in authorisedExt:
+			if ext in authorised_ext:
 				self.http.headers['Content-Type'] = authorised_ext[ext] + "; charset=utf-8"
 				
 				yield open(self.environ['PATH_INFO'][1:]).read(1048576)
